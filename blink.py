60d92923d198d9cb9d90a626cb0ea88532611817
anthony <anthony.h.hornick@gmail.com>
2015-09-29 20:28:52 -0400

    save

--- blink.py
+++ blink.py
@@ -1,31 +1,43 @@
 """my cool lights"""
 
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time as t
 
 pins  = [4,27,22,5,6,13,19,26]
+default_speed= .1
 
-GPIO.setmode(GPIO.BMC)
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins,GPIO.OUT)
 
 def on(pin): GPIO.output(pin,GPIO.HIGH)
 
-def off(pin): GPIO.output(pin,GPIO.LOW
+def off(pin): GPIO.output(pin,GPIO.LOW)
 
-def slowon(list=pins,delay=,1):
+def slowon(list=pins,delay=1):
     for pin in list:
         on(pin)
         t.sleep(delay)
 
-def slowoff(list=pins,delay=,1):
+def slowoff(list=pins,delay=1):
     for pin in list:
         off(pin)
         t.sleep(delay)
 
-def chase(count=1):
+def chase(count=1,delay=default_speed):
     for n in range(count):
-        slowon()
-        slowoff()
+        slowon(delay=delay)
+        slowoff(delay=delay)
+
+def updown(count=1,delay=.1):
+    for n in range(count):
+        slowon(delay=delay)
+        slowoff(delay=delay,list=pins[::-1])
 
 
+patterns = [chase,updown]
 
+
+if __name__ == "__main__":
+    while True:
+        updown(2)
